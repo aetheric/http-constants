ee1d175d4f8d4cf6e95f4ece1f84b2b0a727aeb8
pcummuskey <pcummuskey@bravurasolutions.com>
2015-11-24 10:08:29 +1300

    Adding and updating project config.

---
+++ gulpfiles.js
@@ -0,0 +1,18 @@
+/* global require */
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('build', function() {
+	//
+});
+
+gulp.task('test', function() {
+	//
+});
+
+gulp.task('dist', function() {
+	//
+});
+
+gulp.task('default', [ 'build', 'test' ]);
