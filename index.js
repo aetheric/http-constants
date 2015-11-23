8763dec5d6e0bc4413a588f0fe89cf64089317dd
pcummuskey <pcummuskey@bravurasolutions.com>
2015-11-24 10:23:14 +1300

    Added index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/* global */
+'use strict';
+
+import * as _codes from './src/main/codes.js';
+import * as _headers from './src/main/headers.js';
+import * as _methods from './src/main/methods.js';
+import * as _types from './src/main/types.js';
+
+export const codes = _codes;
+export const headers = _headers;
+export const methods = _methods;
+export const types = _types;
