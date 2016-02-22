ae1cfa994c49ea72eebbe8da621ebf1a5764ed15
Peter Cummuskey <pcummuskey@bravurasolutions.com>
2016-02-22 19:27:44 +1300

    Fixed silly issue with import * instead of default import.

--- src/main/consts-headers.js
+++ src/main/consts-headers.js
@@ -1,8 +1,8 @@
 /* global */
 'use strict';
 
-import * as _request from './consts-headers-request.js';
-import * as _response from './consts-headers-response.js';
+import _request from './consts-headers-request.js';
+import _response from './consts-headers-response.js';
 
 /**
  * @typedef {Object} CONSTSHEADERS
